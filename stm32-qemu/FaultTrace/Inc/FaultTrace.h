cb798afe2b5c96321c5db69a0415d4c7e0340c6f
xidongxu <xi_dongxu@126.com>
2024-04-12 00:08:13 +0800

    【修改】 调试组件；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ stm32-qemu/FaultTrace/Inc/FaultTrace.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file    FaultTrace.h
+  * @brief   This file contains all the function prototypes for
+  *          the FaultTrace.c file
+  ******************************************************************************
+  * @attention
+  *
+  *
+  *
+  ******************************************************************************
+  */
+#ifndef __FaultTrace_H__
+#define __FaultTrace_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FaultTrace_H__ */
+
