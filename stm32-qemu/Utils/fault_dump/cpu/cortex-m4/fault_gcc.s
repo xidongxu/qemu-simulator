ff7adcd861d7d419522f454d6901b71b641db128
xidongxu <xi_dongxu@126.com>
2024-04-16 01:00:41 +0800

    【修改】 gcc工具链异常处理函数；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ stm32-qemu/Utils/fault_dump/cpu/cortex-m4/fault_gcc.s
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @file      fault_gcc.s
+  * @author    MCD Application Team
+  * @brief     STM32F405xx Devices vector table for GCC based toolchains. 
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2017 STMicroelectronics.
+  * All rights reserved.
+  *
+  *
+  *
+  ******************************************************************************
+  */
+
+.syntax unified
+.cpu cortex-m4
+.fpu softvfp
+.thumb
+
+.type HardFault_Handler_Proc, % function
+.global fault_dump_handler
+.global HardFault_Handler_Proc
+HardFault_Handler_Proc:
+    tst     lr, #4
+    ite     eq
+    mrseq   r0, msp
+    mrsne   r0, psp
+    mov     r1, lr
+    ldr     r2, =fault_dump_handler
+    bx      r2
+.end
