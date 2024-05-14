e3b8c4c526a969edaa0c0a12ebee2274e8507527
xidongxu <xi_dongxu@126.com>
2024-05-14 23:49:13 +0800

    【修改】 串口配置；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/mps2-an505/Core/Inc/uart.h
+++ boards/mps2-an505/Core/Inc/uart.h
@@ -1,7 +1,10 @@
 #ifndef UART_H
 #define UART_H
 
+#include <stdio.h>
+
 void uart_init(void);
 void uart_send(const char* string);
+size_t uart_recv(uint8_t* buffer, size_t size);
 
 #endif
--- boards/mps2-an505/Core/Src/uart.c
+++ boards/mps2-an505/Core/Src/uart.c
@@ -1,34 +1,57 @@
 #include "uart.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <stdarg.h>
 
 /* Base address of the UART0 See application note AN505 */
 #define UART0_BASE_NONSECURE    (0x40200000)
 #define UART0_BASE_SECURE       (0x50200000)
 
 #ifdef C_SECURE_CODE
-#define UART0_BASE              UART0_BASE_SECURE
+#define UART0                   UART0_BASE_SECURE
 #else
-#define UART0_BASE              UART0_BASE_NONSECURE
+#define UART0                   UART0_BASE_NONSECURE
 #endif
 
-#define UART_DR(base)           (*((volatile uint32_t *)(base + 0x00)))
+#define UART_DATA(base)         (*((volatile uint32_t *)(base + 0x00)))
+#define UART_RSR(base)          (*((volatile uint32_t *)(base + 0x04)))
 #define UART_CTRL(base)         (*((volatile uint32_t *)(base + 0x08)))
+#define UART_FLAG(base)         (*((volatile uint32_t *)(base + 0x18)))
+#define UART_LPR(base)          (*((volatile uint32_t *)(base + 0x20)))
+#define UART_IBRD(base)         (*((volatile uint32_t *)(base + 0x24)))
+#define UART_FBRD(base)         (*((volatile uint32_t *)(base + 0x28)))
+#define UART_LCRH(base)         (*((volatile uint32_t *)(base + 0x2C)))
+#define UART_CR(base)           (*((volatile uint32_t *)(base + 0x30)))
+#define UART_IFLS(base)         (*((volatile uint32_t *)(base + 0x34)))
+#define UART_MSC(base)          (*((volatile uint32_t *)(base + 0x38)))
+#define UART_RIS(base)          (*((volatile uint32_t *)(base + 0x3C)))
+#define UART_MIS(base)          (*((volatile uint32_t *)(base + 0x40)))
+#define UART_ICR(base)          (*((volatile uint32_t *)(base + 0x44)))
+#define UART_DMACR(base)        (*((volatile uint32_t *)(base + 0x48)))
 
 #ifdef __GNUC__
-#define PUTCHAR_PROTOTYPE int putchar(int ch)
+#define PUTCHAR_PROTOTYPE       int putchar(int ch)
+#define GETCHAR_PROTOTYPE       int getchar(void)
 #else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#define PUTCHAR_PROTOTYPE       int fputc(int ch, FILE *f)
+#define GETCHAR_PROTOTYPE       int fgetc(FILE *f)
 #endif
 
 PUTCHAR_PROTOTYPE {
-    UART_DR(UART0_BASE) = ch;
+    while (UART_FLAG(UART0) & 0x20);
+    UART_DATA(UART0) = ch;
     return ch;
 }
 
+GETCHAR_PROTOTYPE {
+    while ((UART_FLAG(UART0) & 0x40) == 0);
+    return UART_DATA(UART0);
+}
+
 void uart_init(void) {
-    UART_CTRL(UART0_BASE) = 1;
+    UART_IBRD(UART0) = 40;
+    UART_FBRD(UART0) = 11;
+    UART_LCRH(UART0) = 0x60;
+    UART_CR(UART0)   = 0x301;
+    UART_RSR(UART0)  = 0x0A;
+    UART_CTRL(UART0) = 0x01;
 }
 
 void uart_send(const char* string) {
@@ -36,4 +59,18 @@ void uart_send(const char* string) {
         putchar(*string);
         string++;
     }
+}
+
+size_t uart_recv(uint8_t* buffer, size_t size) {
+    uint8_t ch = 0;
+    size_t it = 0;
+    while (1) {
+        ch = getchar();
+        if(ch == '\n' || it >= size) {
+            break;
+        }
+        buffer[it] = ch;
+        it = it + 1;
+    }
+    return (it + 1);
 }
