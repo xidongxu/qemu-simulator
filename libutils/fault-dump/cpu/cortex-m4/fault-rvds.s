5d8aa9af559afd45252f5ec2a0b187a02419da22
xidongxu <xi_dongxu@126.com>
2024-05-17 00:29:51 +0800

    【修改】 同步 Hardfault_Handler 的处理方式至 stm32工程中；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- libutils/fault-dump/cpu/cortex-m4/fault-rvds.s
+++ libutils/fault-dump/cpu/cortex-m4/fault-rvds.s
@@ -12,15 +12,34 @@
 
     AREA |.text|, CODE, READONLY, ALIGN=2
     THUMB
+    IMPORT fd_code_stack_base
+    IMPORT fd_code_stack_full
     IMPORT fault_dump_handler
 HardFault_Handler    PROC
     EXPORT  HardFault_Handler
-    TST    LR, #4
-    ITE    EQ
-    MRSEQ  R0, MSP
-    MRSNE  R0, PSP
-    STMFD  R0!, {R4 - R11}
-    MOV    R1, LR
-    BL     fault_dump_handler
+    LDR     R3, =fd_code_stack_base     ; r3 = &fd_code_stack_base
+    LDR     R1, [R3]                    ; r1 = fd_code_stack_base
+    LDR     R3, =fd_code_stack_full     ; r3 = &fd_code_stack_full
+    LDR     R2, [R3]                    ; r2 = fd_code_stack_full
+
+    TST     LR, #4                      ; use exc_return value get sp value
+    ITE     EQ
+    MRSEQ   R0, MSP                     ; lr & 0x04 == 1, r0 = sp = msp
+    MRSNE   R0, PSP                     ; lr & 0x04 == 0, r0 = sp = psp
+
+    CMP     R0, R1                      ; compare sp and stack base addr
+    BLT     stack_invalid               ; sp < stack_base_addr, sp is invalid
+    CMP     R0, R2                      ; compare sp and stack full addr
+    BGT     stack_invalid               ; sp > stack_base_addr, sp is invalid
+
+    PUSH    {R4 - R11}                  ; sp value is in stack range, push r4 - r11 to stack
+
+    TST     LR, #4                      ; update sp value to r0
+    ITE     EQ
+    MRSEQ   R0, MSP                     ; lr & 0x04 == 1, r0 = sp = msp
+    MRSNE   R0, PSP                     ; lr & 0x04 == 0, r0 = sp = psp
+stack_invalid
+    MOV     R1, LR                      ; now, r0 = sp, r1 = lr
+    BL     fault_dump_handler           ; jump to fault_dump_handler
     ENDP
     END
