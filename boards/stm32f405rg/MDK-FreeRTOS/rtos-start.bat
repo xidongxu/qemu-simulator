0478bfffffe1c3060f48f22a6a9eac6ce3242543
xidongxu <xi_dongxu@126.com>
2024-05-22 00:46:53 +0800

    【修改】 调试脚本名称；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/MDK-FreeRTOS/rtos-start.bat
+++ boards/stm32f405rg/MDK-FreeRTOS/rtos-start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting QEMU...
-start "" "bare-qemu.bat"
+start "" "rtos-qemu.bat"
 echo Starting GDB...
-start "" "bare-gdb.bat"
+start "" "rtos-gdb.bat"
 echo All scripts started.
