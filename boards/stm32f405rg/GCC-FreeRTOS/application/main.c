7d1290f0dd3a067da6b41affca830b66ed2e07c3
xidongxu <xi_dongxu@126.com>
2024-05-27 22:15:47 +0800

    【修改】 GCC-FreeRTOS 工程 main.c文件；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/GCC-FreeRTOS/application/main.c
+++ boards/stm32f405rg/GCC-FreeRTOS/application/main.c
@@ -1,4 +1,3 @@
-/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.c
@@ -6,263 +5,165 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+
 #include "usart.h"
 #include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_it.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "fault-dump.h"
-/* USER CODE END Includes */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <queue.h>
 #include <timers.h>
-/* USER CODE END PTD */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
+uint8_t ucHeap[configTOTAL_HEAP_SIZE];
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-uint8_t ucHeap[ configTOTAL_HEAP_SIZE ];
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
 #ifdef __GNUC__
-/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
- set to 'Yes') calls __io_putchar() */
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
 #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
-/**
-  * @brief  Retargets the C library printf function to the USART.
-  * @param  None
-  * @retval None
-  */
-PUTCHAR_PROTOTYPE
-{
-  /* Place your implementation of fputc here */
-  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
-  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
-  return ch;
+#endif
+PUTCHAR_PROTOTYPE {
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+    return ch;
 }
 
-void fault_div_zero_trigger(void) 
-{
-  int a = 0, b = 0, c = 0;
+void fault_div_zero_trigger(void) {
+    int a = 0, b = 0, c = 0;
 
-  SCB->CCR |= SCB_CCR_DIV_0_TRP_Msk;
-  c = (a + (b / c));
-  printf("c = %d\r\n", c);
+    SCB->CCR |= SCB_CCR_DIV_0_TRP_Msk;
+    c = (a + (b / c));
+    printf("c = %d\r\n", c);
 }
 
-void fault_unalign_trigger(void) 
-{
-  volatile int *addr = NULL;
-  volatile int value = 0;
-  SCB->CCR |= SCB_CCR_UNALIGN_TRP_Msk;
+void fault_unalign_trigger(void) {
+    volatile int *addr = NULL;
+    volatile int value = 0;
+    SCB->CCR |= SCB_CCR_UNALIGN_TRP_Msk;
 
-  addr = (int*)0x00;
-  value = *addr;
-  printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
-  addr = (int*)0x04;
-  value = *addr;
-  printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
-  addr = (int*)0x03;
-  value = *addr;
-  printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
+    addr = (int*)0x00;
+    value = *addr;
+    printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
+    addr = (int*)0x04;
+    value = *addr;
+    printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
+    addr = (int*)0x03;
+    value = *addr;
+    printf("addr:0x%02X-value:0x%08X\r\n", (int)addr, value);
 }
 
-void test0(void)
-{
-  printf("this is %s.\r\n", __func__);
-  // trigger a fault.
-  //float a = 0.0, b = 1.1;
-  //printf("this is %f.\r\n", (a + b));
-  fault_unalign_trigger();
+void test0(void) {
+    printf("this is %s.\r\n", __func__);
+    // trigger a fault.
+    //float a = 0.0, b = 1.1;
+    //printf("this is %f.\r\n", (a + b));
+    fault_unalign_trigger();
 }
 
-void test1(void)
-{
-  printf("this is %s.\r\n", __func__);
-  test0();
+void test1(void) {
+    printf("this is %s.\r\n", __func__);
+    test0();
 }
 
-void test2(void)
-{
-  printf("this is %s.\r\n", __func__);
-  test1();
+void test2(void) {
+    printf("this is %s.\r\n", __func__);
+    test1();
 }
 
-void test3(void)
-{
-  printf("this is %s.\r\n", __func__);
-  test2();
+void test3(void) {
+    printf("this is %s.\r\n", __func__);
+    test2();
 }
 
-void test4(void)
-{
-  printf("this is %s.\r\n", __func__);
-  test3();
+void test4(void) {
+    printf("this is %s.\r\n", __func__);
+    test3();
 }
 
-void test5(void)
-{
-  printf("this is %s.\r\n", __func__);
-  test4();
+void test5(void) {
+    printf("this is %s.\r\n", __func__);
+    test4();
 }
 
-static void exampleTask(void *parameters)
-{
-    while(1) 
-    {
+static void main_task_entry(void *parameters) {
+    while(1) {
         vTaskDelay(1000);
         printf("hello this is FreeRTOS.\r\n");
     }
 }
 
-static void exampleInit(void)
-{
-    static TaskHandle_t AppTask = NULL;
+static void main_task_init(void) {
+    static TaskHandle_t main_task = NULL;
     BaseType_t xReturn = pdPASS;
-    xReturn = xTaskCreate(exampleTask, "example", 2048, NULL, 1U, &AppTask);
+    xReturn = xTaskCreate(main_task_entry, "main_task", 2048, NULL, 1U, &main_task);
     if (xReturn == pdPASS) {
         vTaskStartScheduler();
     } else {
-        printf("example task create failed(%d).\r\n", xReturn);
+        printf("main task create failed(%d).\r\n", (int)(xReturn));
     }
 }
-/* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+int main(void) {
+    HAL_Init();
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
 
-  /* USER CODE BEGIN 1 */
+    fault_dump_init();
+    main_task_init();
+    // test5();
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  // SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
-  fault_dump_init();
-  exampleInit();
-  //test5();
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    printf("%s", "hello qemu.\r\n");
-    HAL_Delay(1000);
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    while (1) {
+        printf("%s", "hello qemu.\r\n");
+        HAL_Delay(1000);
+    }
+    return 0;
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 168;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+        Error_Handler();
+    }
 }
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
