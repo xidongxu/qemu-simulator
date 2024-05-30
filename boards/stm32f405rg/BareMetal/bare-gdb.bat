1e90be5c70fc5d2b1ec7161591772ddd776709ec
xidongxu <xi_dongxu@126.com>
2024-05-31 00:35:54 +0800

    【修改】 整理BareMetal文件夹，可以正常编译工程；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/BareMetal/bare-gdb.bat
+++ boards/stm32f405rg/BareMetal/bare-gdb.bat
@@ -1,2 +1,28 @@
-arm-none-eabi-gdb.exe -ex "target remote localhost:1234" -ex "file ./build/stm32-qemu.elf" -ex "break main" -ex "continue"
+@echo off
+
+set tool=%1
+set file=""
+set exec=""
+
+if %tool% == gcc (
+    set file="./build/stm32-qemu.elf"
+    set exec="file ./build/stm32-qemu.elf"
+) 
+if %tool% == iar (
+    set file="./build/stm32-qemu.out"
+    set exec="file ./build/stm32-qemu.out"
+) 
+if %tool% == mdk (
+    set file="./build/stm32-qemu.axf"
+    set exec="file ./build/stm32-qemu.axf"
+) 
+if not exist %file% (
+    echo Executable file not found for tool: %1
+    goto finish
+)
+
+echo executable file is exist, start gdb:
+arm-none-eabi-gdb.exe -ex "target remote localhost:1234" -ex %exec% -ex "break main" -ex "continue"
+
+:finish
 pause
--- boards/stm32f405rg/BareMetal/bare-start.bat
+++ boards/stm32f405rg/BareMetal/bare-start.bat
@@ -1,6 +1,7 @@
 @echo off
-echo Starting QEMU...
-start "" "bare-qemu.bat"
-echo Starting GDB...
-start "" "bare-gdb.bat"
+
+echo starting %1 qemu
+start "" "bare-qemu.bat" %1
+echo starting %1 gdb
+start "" "bare-gdb.bat" %1
 echo All scripts started.
