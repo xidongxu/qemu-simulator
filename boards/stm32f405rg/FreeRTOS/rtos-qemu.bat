131b66fb6831428684edbf089c8d2f778e7fd544
xidongxu <xi_dongxu@126.com>
2024-06-01 14:39:57 +0800

    【修改】 FreeRTOS工程可以正常编译通过且正常运行；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/FreeRTOS/rtos-qemu.bat
+++ boards/stm32f405rg/FreeRTOS/rtos-qemu.bat
@@ -1,2 +1,24 @@
-qemu-system-arm.exe -M netduinoplus2 -nographic -kernel ./build/stm32-qemu.bin -S -s
+@echo off
+
+set tool=%1
+set file=""
+
+if %tool% == gcc (
+    set file=./build/gcc/stm32-qemu.elf
+) 
+if %tool% == iar (
+    set file=./build/iar/stm32-qemu.out
+) 
+if %tool% == mdk (
+    set file=./build/mdk/stm32-qemu.axf
+) 
+if not exist "%file%" (
+    echo executable file not found for tool: %1
+    goto finish
+)
+
+echo executable file is exist, start qemu:
+qemu-system-arm.exe -M netduinoplus2 -nographic -kernel %file% -S -s
+
+:finish
 pause
--- boards/stm32f405rg/FreeRTOS/rtos-start.bat
+++ boards/stm32f405rg/FreeRTOS/rtos-start.bat
@@ -1,6 +1,7 @@
 @echo off
-echo Starting QEMU...
-start "" "rtos-qemu.bat"
-echo Starting GDB...
-start "" "rtos-gdb.bat"
+
+echo starting %1 qemu
+start "" "rtos-qemu.bat" %1
+echo starting %1 gdb
+start "" "rtos-gdb.bat" %1
 echo All scripts started.
