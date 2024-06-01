806e00307e98a77dcd55cb05f05691a8094def00
xidongxu <xi_dongxu@126.com>
2024-06-01 17:39:49 +0800

    【修改】 删除 IAR 与 MDK 的 FreeRTOS 目录；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/MDK-FreeRTOS/rtos-gdb.bat
+++
@@ -1,2 +0,0 @@
-arm-none-eabi-gdb.exe -ex "target remote localhost:1234" -ex "file ./stm32-qemu/stm32-qemu.axf" -ex "break main" -ex "continue"
-pause
--- boards/stm32f405rg/MDK-FreeRTOS/rtos-qemu.bat
+++
@@ -1,2 +0,0 @@
-qemu-system-arm.exe -M netduinoplus2 -nographic -kernel ./stm32-qemu/stm32-qemu.axf -S -s
-pause
