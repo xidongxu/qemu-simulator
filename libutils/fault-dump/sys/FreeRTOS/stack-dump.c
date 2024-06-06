32963800ab058c9ffe237e5d05a5d0b629ed3dce
xidongxu <xi_dongxu@126.com>
2024-06-06 22:00:46 +0800

    【修改】 stack-dump.c文件；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ libutils/fault-dump/sys/FreeRTOS/stack-dump.c
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    stack-dump.c
+  * @author  xidongxu
+  * @brief   This file contains all the function prototypes for the stack-dump.c
+  ******************************************************************************
+  * @attention
+  *
+  *
+  *
+  ******************************************************************************
+  */
+#include "fault-dump.h"
+
+void freertos_stack_dump(unsigned int exc_return) {
+    return;
+}
