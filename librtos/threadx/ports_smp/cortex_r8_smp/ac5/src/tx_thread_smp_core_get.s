44f723a2285226389edfaefbdb0a9266b7ae66cb
xidongxu <xi_dongxu@126.com>
2024-08-14 23:22:34 +0800

    【修改】 threadx源码；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ librtos/threadx/ports_smp/cortex_r8_smp/ac5/src/tx_thread_smp_core_get.s
@@ -0,0 +1,71 @@
+/***************************************************************************
+ * Copyright (c) 2024 Microsoft Corporation 
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which is available at
+ * https://opensource.org/licenses/MIT.
+ * 
+ * SPDX-License-Identifier: MIT
+ **************************************************************************/
+
+
+/**************************************************************************/
+/**************************************************************************/
+/**                                                                       */
+/** ThreadX Component                                                     */
+/**                                                                       */
+/**   Thread                                                              */
+/**                                                                       */
+/**************************************************************************/
+/**************************************************************************/
+    AREA ||.text||, CODE, READONLY
+        PRESERVE8
+/**************************************************************************/
+/*                                                                        */
+/*  FUNCTION                                               RELEASE        */
+/*                                                                        */
+/*    _tx_thread_smp_core_get                         SMP/Cortex-R8/ARM   */
+/*                                                           6.2.0        */
+/*  AUTHOR                                                                */
+/*                                                                        */
+/*    Scott Larson, Microsoft Corporation                                 */
+/*                                                                        */
+/*  DESCRIPTION                                                           */
+/*                                                                        */
+/*    This function gets the currently running core number and returns it.*/
+/*                                                                        */
+/*  INPUT                                                                 */
+/*                                                                        */
+/*    None                                                                */
+/*                                                                        */
+/*  OUTPUT                                                                */
+/*                                                                        */
+/*    Core ID                                                             */
+/*                                                                        */
+/*  CALLS                                                                 */
+/*                                                                        */
+/*    None                                                                */
+/*                                                                        */
+/*  CALLED BY                                                             */
+/*                                                                        */
+/*    ThreadX Source                                                      */
+/*                                                                        */
+/*  RELEASE HISTORY                                                       */
+/*                                                                        */
+/*    DATE              NAME                      DESCRIPTION             */
+/*                                                                        */
+/*  10-31-2022      Scott Larson            Initial Version 6.2.0         */
+/*                                                                        */
+/**************************************************************************/
+    EXPORT  _tx_thread_smp_core_get
+_tx_thread_smp_core_get
+    MRC     p15, 0, r0, c0, c0, 5               // Read CPU ID register
+    AND     r0, r0, #0x03                       // Mask off, leaving the CPU ID field
+
+    IF  {INTER} = {TRUE}
+    BX      lr                                  // Return to caller
+    ELSE
+    MOV     pc, lr                              // Return to caller
+    ENDIF
+
+    END
