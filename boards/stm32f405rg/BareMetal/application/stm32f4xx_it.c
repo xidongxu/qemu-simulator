560ec5a7a9f16011cea7200a1a783fbcfafb5486
xidongxu <xi_dongxu@126.com>
2024-08-21 23:22:15 +0800

    【修改】 stm32f405rg的HAL库时基使用TIM2;
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- boards/stm32f405rg/BareMetal/application/stm32f4xx_it.c
+++ boards/stm32f405rg/BareMetal/application/stm32f4xx_it.c
@@ -1,4 +1,4 @@
-/* USER CODE BEGIN Header */
+﻿/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file    stm32f4xx_it.c
@@ -57,7 +57,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef huart1;
-extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN EV */
 
@@ -202,17 +202,17 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
+  * @brief This function handles TIM2 global interrupt.
   */
-void TIM1_UP_TIM10_IRQHandler(void)
+void TIM2_IRQHandler(void)
 {
-  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
 
-  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim1);
-  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
-
-  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+  /* USER CODE END TIM2_IRQn 1 */
 }
 
 /**
